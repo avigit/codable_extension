85d8a1ed37f28a4a20652f7062419fd84b145b13
Avigit Saha <me@avigit.com>
2019-11-11 13:57:53 -0800

    Initial Commit

---
+++ Codable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Codable
+//
+//  Created by Avigit Saha on 11/11/19.
+//  Copyright © 2019 Avigit Saha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
